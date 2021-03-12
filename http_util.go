f74595548ec36d5f42dc3c37cf85baf306f211b2
aljo242 <amj92@pitt.edu>
2021-03-11 20:57:43 -0500

    add CheckHTTP2Support()

--- http_util.go
+++ http_util.go
@@ -1,8 +1,24 @@
 package http_util
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
 
 func Print() error {
 	fmt.Println("vim-go")
 	return nil
+}
+
+// CheckHTTP2Support is a simple test to see if HTTP2 is supported by checking if http.Pusher is in the responsewriter
+func CheckHTTP2Support(w http.ResponseWriter) bool {
+	_, ok := w.(http.Pusher)
+	if ok {
+		log.Printf("HTTP/2 Supported!\n")
+	} else {
+		log.Printf("HTTP/2 NOT Supported!\n")
+	}
+
+	return ok
 }
